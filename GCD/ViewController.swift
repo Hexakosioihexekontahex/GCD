9fe083f154e8ae7b9a56b576a00c8322d45df240
Hex <wolfgunmiller@gmail.com>
2020-02-27 16:00:23 +0300

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Roman Melnik on 27.02.2020.
+//  Copyright © 2020 Roman Melnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
